142ada36e9cb950db0d6940774f86a5735f5c12a
Slav <itiksolution@gmail.com>
2022-08-09 14:44:06 +1000

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+  for (let i = 0; i < name.length; i++) {
+    name[i] = (`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+  }
+  return name
+}
+writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
+
+
+function countDown() {
+  let i = 10
+  while (i >= 0) {
+    console.log(i--)
+  }
+  return i
+}
+
